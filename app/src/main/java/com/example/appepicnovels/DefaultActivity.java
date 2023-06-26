dc667504f21a28e595c82b152692f7ea9766693a
minhdev-ui <dominh162002@gmail.com>
2023-06-26 11:13:28 +0700

    update ui

--- app/src/main/java/com/example/appepicnovels/DefaultActivity.java
+++ app/src/main/java/com/example/appepicnovels/DefaultActivity.java
@@ -4,6 +4,7 @@ import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class DefaultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.default_layout);
         LocalActivityManager activityManager = new LocalActivityManager(this, false);
         activityManager.dispatchCreate(savedInstanceState);
--- app/src/main/java/com/example/appepicnovels/DiscoverActivity.java
+++ app/src/main/java/com/example/appepicnovels/DiscoverActivity.java
@@ -1,61 +1,91 @@
 package com.example.appepicnovels;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.GridView;
-
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.appepicnovels.adapters.DiscoverAdapter;
 import com.example.appepicnovels.interfaces.StoriesCallback;
 import com.example.appepicnovels.models.Story;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.*;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 public class DiscoverActivity extends AppCompatActivity {
-GridView grvListStory;
-DiscoverAdapter adapter;
-ArrayList<Story> DiscoverArrayList;
+    GridView grvListStory;
+    DiscoverAdapter adapter;
+    ArrayList<Story> DiscoverArrayList;
+    ProgressBar progressBar;
+
+    private Handler handler = new Handler();
+    private Handler handlerProcess = new Handler();
+    private static final int DEBOUNCE_DELAY = 1000;
+
+    long updateIntervalInMillis = (long) DEBOUNCE_DELAY / 90;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+        progressBar = findViewById(R.id.loading_view);
         init();
         setClick();
     }
 
     public void getAllStories(final StoriesCallback callback) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference usersCollection = db.collection("Stories");
-        Task<QuerySnapshot> query = usersCollection.get();
-        ArrayList<Story> storyArrayList = new ArrayList<>();
-        query.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        progressBar.setVisibility(View.VISIBLE);
+        handlerProcess.removeCallbacksAndMessages(null);
+        handlerProcess.postDelayed(new Runnable() {
+            int currentProcess = 0;
             @Override
-            public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()) {
-                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                        storyArrayList.add(new Story(documentSnapshot.getString("name"), documentSnapshot.getString("lastestChap"), documentSnapshot.getString("img")));
-                    }
-                    callback.onStoriesLoad(storyArrayList);
-                } else {
-                    Toast.makeText(DiscoverActivity.this, "Server Error", Toast.LENGTH_SHORT).show();
-                }
+            public void run() {
+                progressBar.setProgress(currentProcess);
+                currentProcess++;
+                handlerProcess.postDelayed(this, updateIntervalInMillis);
             }
-        });
-    };
+        }, updateIntervalInMillis);
+        handler.removeCallbacksAndMessages(null);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                CollectionReference usersCollection = db.collection("Stories");
+                Task<QuerySnapshot> query = usersCollection.get();
+                ArrayList<Story> storyArrayList = new ArrayList<>();
+                query.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot queryDocument = task.getResult();
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                storyArrayList.add(new Story(documentSnapshot.getString("name"), documentSnapshot.getString("lastestChap"), documentSnapshot.getString("img")));
+                            }
+                            callback.onStoriesLoad(storyArrayList);
+                        } else {
+                            Toast.makeText(DiscoverActivity.this, "Server Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }, DEBOUNCE_DELAY);
+    }
 
-    private void init(){
+    ;
+
+    private void init() {
         DiscoverArrayList = new ArrayList<>();
         grvListStory = findViewById(R.id.grvListStory);
+        grvListStory.setVisibility(View.GONE);
         getAllStories(new StoriesCallback() {
             @Override
             public void onStoriesLoad(ArrayList<Story> stories) {
@@ -64,9 +94,15 @@ ArrayList<Story> DiscoverArrayList;
                 }
                 adapter = new DiscoverAdapter(DiscoverActivity.this, 0, DiscoverArrayList);
                 grvListStory.setAdapter(adapter);
+                progressBar.setProgress(100);
+                progressBar.setVisibility(View.GONE);
+                grvListStory.setVisibility(View.VISIBLE);
             }
         });
     }
 
-    private void setClick(){};
+    private void setClick() {
+    }
+
+    ;
 }
